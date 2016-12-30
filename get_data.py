bcc9c72eedf8d5612bbd78c5cca181680e45927f
ToluwaniO <ogunsanyatoluwani@gmail.com>
2016-12-30 18:41:21 -0500

    documentation

--- get_data.py
+++ get_data.py
@@ -1,6 +1,8 @@
 import requests
 
 def get_from_web():
+    '''() -> list of dictionaries
+    This method returns all the json pages as a list of dictionaries'''
     pages = []
     for i in range(1,4):
         url = 'https://shopicruit.myshopify.com/admin/orders.json?page=' + str(i) +'&access_token=c32313df0d0ef512ca64d5b336a0d7c6'
@@ -12,6 +14,9 @@ def get_from_web():
     return pages
 
 def get_total_values(pages):
+    '''(list of dictionaries) -> tuple of floats
+    This method takes the list of dictinaries with data and returns a tuple with the total made,
+    total paid by customers and total tax by customers'''
     total_paid = 0
     total_tax = 0
 
@@ -19,7 +24,6 @@ def get_total_values(pages):
         for j in i['orders']:
             total_paid += float(j['total_price'])
             total_tax += float(j['total_tax'])
-            #print(str(total_paid))
 
     return (total_paid+total_tax, round(total_paid,2), total_tax)
 
